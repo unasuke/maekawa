97f96a460a9c3eff5b9470ce8e4fe140e5f4a1ba
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-02-22 03:22:16 +0900

    Create func AssociateRules

--- builder.go
+++ builder.go
@@ -4,6 +4,22 @@ import (
 	"github.com/aws/aws-sdk-go/service/cloudwatchevents"
 )
 
+// Associate ClowdWatchEvent Rule and descripbed Rule(name based)
+func AssociateRules(cweRules []*cloudwatchevents.Rule, describedRules []Rule) {
+	dupCWERules := make([]*cloudwatchevents.Rule, cap(cweRules))
+	copy(dupCWERules, cweRules)
+
+	for i, rule := range describedRules {
+		for j, cweRule := range dupCWERules {
+			if *cweRule.Name == rule.Name {
+				describedRules[i].ActualRule = *cweRule
+				dupCWERules = DeleteRuleFromSlice(dupCWERules, j)
+				break
+			}
+		}
+	}
+}
+
 // return ClowdWatchEvent Rules that deleted specified index rule.
 func DeleteRuleFromSlice(src []*cloudwatchevents.Rule, deleteIndex int) []*cloudwatchevents.Rule {
 	dest := []*cloudwatchevents.Rule{}
--- builder_test.go
+++ builder_test.go
@@ -7,6 +7,52 @@ import (
 	"github.com/aws/aws-sdk-go/service/cloudwatchevents"
 )
 
+func TestAssociateRules(t *testing.T) {
+	cweRules := []*cloudwatchevents.Rule{
+		&cloudwatchevents.Rule{
+			Arn:                aws.String("arn:aws:events:ap-northeast-1:000000000000:rule/test-1"),
+			Description:        aws.String("Test rule 1"),
+			EventPattern:       nil,
+			Name:               aws.String("test-1"),
+			RoleArn:            nil,
+			ScheduleExpression: aws.String("cron(0 20 * * ? *)"),
+			State:              aws.String("ENABLED"),
+		},
+		&cloudwatchevents.Rule{
+			Arn:                aws.String("arn:aws:events:ap-northeast-1:000000000000:rule/test-2"),
+			Description:        aws.String("Test rule 2"),
+			EventPattern:       nil,
+			Name:               aws.String("test-2"),
+			RoleArn:            nil,
+			ScheduleExpression: aws.String("cron(0 20 2 * ? *)"),
+			State:              aws.String("ENABLED"),
+		},
+	}
+	describedRules := []Rule{
+		Rule{
+			Description:        "Test rule 1",
+			EventPattern:       "",
+			Name:               "test-1",
+			ScheduleExpression: "cron(0 20 * * ? *)",
+			State:              "ENABLED",
+		},
+		Rule{
+			Description:        "Test rule 2",
+			EventPattern:       "",
+			Name:               "test-2",
+			ScheduleExpression: "cron(0 20 2 * ? *)",
+			State:              "ENABLED",
+		},
+	}
+	AssociateRules(cweRules, describedRules)
+	if *describedRules[0].ActualRule.Name != "test-1" {
+		t.Errorf("should associate 'test-1'")
+	}
+	if *describedRules[1].ActualRule.Name != "test-2" {
+		t.Errorf("should associate 'test-2'")
+	}
+}
+
 func TestDeleteRuleFromSlice(t *testing.T) {
 	cweRules := []*cloudwatchevents.Rule{
 		&cloudwatchevents.Rule{
